2a8bdd38616a372391b5e328e29a7f35206b7a60
saidu007 <57762448+saidu007@users.noreply.github.com>
2020-08-18 21:15:46 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,3 +2,5 @@
 mvn clean compile install
 sudo docker build -f Dockerfile -t product .
 sudo docker run -p 8081:8081 product &
+sudo docke tag product:1.2 saidu007/product:latest
+sudo docker push saidu007/product:latest
