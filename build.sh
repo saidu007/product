6c6b03645df62d6e0a938998a671163a5d168c85
saidu007 <57762448+saidu007@users.noreply.github.com>
2020-08-19 14:34:39 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ sudo docker build -f Dockerfile -t product .
 sudo docker run -p 8081:8081 product &
 sudo docke tag product:1.2 saidu007/product:latest
 sudo docker push saidu007/product:latest
+kubectl apply -f Java.yaml
